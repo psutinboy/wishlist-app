7e3b4b3b7642bc5c21b68ee58cf0c1f96097152d
Psutinboy <grantledbetter12@gmail.com>
2025-11-13 11:26:47 -0600

    commit 2

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,30 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'list/:id',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'l/:shareId',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'login',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'signup',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'dashboard',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'settings',
+    renderMode: RenderMode.Prerender
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
